1699124dfd23c00ec9783e9a35d6fe8f258987e9
Kevin SeungWhan Chung <schung58@illinois.edu>
2016-08-22 12:53:54 -0500

    Time integration, QoI

--- Brownian.py
+++ Brownian.py
@@ -12,14 +12,36 @@ class Brownian(object):
 		self.l = l
 		self.dt = dt
 
-		self.x0, self.y0 = x0, y0
+		self.x, self.y = x0, y0
 		if x0 is None:
-			self.x0 = np.zeros(N, dtype=self.dtype)
+			self.x = np.zeros(N, dtype=self.dtype)
 		if y0 is None:
-			slef.y0 = np.zeros(N, dtype=self.dtype)
+			self.y = np.zeros(N, dtype=self.dtype)
 
 	def move(self):
 
-		theta = 2.0*np.pi*np.random.rand(N)
-		x0 += self.l*np.cos(theta)
-		y0 += self.l*np.sin(theta)
+		theta = 2.0*np.pi*np.random.rand(self.N)
+		self.x += self.l*np.cos(theta)
+		self.y += self.l*np.sin(theta)
+
+class TimeIntegrator(object):
+    
+    def __init__(self,Brownian,Nt):
+        self.N = Brownian.N
+        self.Nt = Nt
+        self.xData = np.zeros((Brownian.N,Nt),dtype=Brownian.dtype)
+        self.yData = np.zeros((Brownian.N,Nt),dtype=Brownian.dtype)
+        
+    def forward(self,Brownian):
+        for i in range(1,self.Nt):
+            Brownian.move()
+            self.xData[:,i]=Brownian.x
+            self.yData[:,i]=Brownian.y
+            
+class QoI(object):
+    
+    def __init__(self):
+        self.J=0.0
+        
+    def compute_J(self,TimeIntegrator):
+        self.J = np.mean( np.square(TimeIntegrator.xData)+np.square(TimeIntegrator.yData) )
