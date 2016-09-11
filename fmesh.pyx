1d98c4d1bbf2c40cc33e65b5c3c61a566c7dfa13
Kevin SeungWhan Chung <schung58@illinois.edu>
2016-09-11 18:20:42 -0500

    fortran wrapper simplified

--- fmesh.pyx
+++ fmesh.pyx
@@ -9,21 +9,6 @@ cdef extern:
     void c_pdfgrad(int *Ng, double *dx, double *pg,
                     double *pg_dx, double *pg_dy)
 
-def f_ptc2pdf(int Ng,
-                double dx,
-                int N,
-                double[:] x, double[:] y, double[:] spwt_input):
-    cdef np.ndarray[DOUBLE,ndim=2] pg = np.zeros((2*Ng+1,2*Ng+1),dtype=np.double)
-    c_ptc2pdf(&Ng,&dx,&N,&pg[0,0],&x[0],&y[0],&spwt_input[0])
-    return pg
-
-def f_pdfgrad(int Ng, double dx,
-                double[:,:] pg):
-    cdef np.ndarray[DOUBLE,ndim=2] pg_dx = np.zeros((2*Ng+1,2*Ng+1),dtype=np.double)
-    cdef np.ndarray[DOUBLE,ndim=2] pg_dy = np.zeros((2*Ng+1,2*Ng+1),dtype=np.double)
-    c_pdfgrad(&Ng,&dx,&pg[0,0],&pg_dx[0,0],&pg_dy[0,0])
-    return pg_dx, pg_dy
-
 class mesh(object):
     
     dtype = np.double
@@ -35,13 +20,26 @@ class mesh(object):
         self.xg = np.linspace(-L,L,2*Ng+1)
         self.yg = np.linspace(-L,L,2*Ng+1)
 
-    def ptc2pdf(self,x,y,spwt=None):
+    def ptc2pdf(self,
+                double[:] x, double[:] y,
+                double[:] spwt=None):
+        cdef int N, Ng
+        cdef double dx
         N = len(x)
+        Ng = self.Ng
+        dx = self.dx
+        cdef np.ndarray[DOUBLE,ndim=2] pg = np.zeros((2*self.Ng+1,2*self.Ng+1),dtype=np.double)
         if spwt is None:
             spwt = 1./N*np.ones(N)
-        pg = f_ptc2pdf(self.Ng,self.dx,N,x,y,spwt)
+        c_ptc2pdf(&Ng,&dx,&N,&pg[0,0],&x[0],&y[0],&spwt[0])
         return pg
     
-    def pdfgrad(self,pg):
-        pg_dx, pg_dy = f_pdfgrad(self.Ng,self.dx,pg)
+    def pdfgrad(self,double[:,:] pg):
+        cdef int Ng
+        cdef double dx
+        Ng = self.Ng
+        dx = self.dx
+        cdef np.ndarray[DOUBLE,ndim=2] pg_dx = np.zeros((2*Ng+1,2*Ng+1),dtype=np.double)
+        cdef np.ndarray[DOUBLE,ndim=2] pg_dy = np.zeros((2*Ng+1,2*Ng+1),dtype=np.double)
+        c_pdfgrad(&Ng,&dx,&pg[0,0],&pg_dx[0,0],&pg_dy[0,0])
         return pg_dx, pg_dy
